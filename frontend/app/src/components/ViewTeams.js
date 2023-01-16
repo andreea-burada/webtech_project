183e494c09e122361aedad6c740bda71aba6d19a
Andreea B <burada.andreea@outlook.com>
2023-01-16 23:08:05 +0200

    File cleanup

--- frontend/app/src/components/ViewTeams.js
+++ frontend/app/src/components/ViewTeams.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import {FaSearch} from 'react-icons/fa'
 import axios from 'axios';
-//import Teams from './teamInfoSummary.json';
-// import './ViewTeams.css';
 import '../App.css';
-import './Teams.css';
+import './ViewTeams.css';
 import { Link } from 'react-router-dom';
 
 var Teams = [];
